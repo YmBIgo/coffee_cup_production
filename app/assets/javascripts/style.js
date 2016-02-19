7f2617c257fe6482c4f920a952959aefa64f393a
Kazuya <IgYmB0218LP@gmail.com>
2016-02-19 23:49:17 +0900

    fix fixed side

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -2,10 +2,12 @@ $(function(){
   var fix    = $("#slide-bar");             //メニューオブジェクトを格納
   var fixTop = fix.offset().top;             //メニューの縦座標を格納
   $(window).scroll(function () {             //スクロールが発生したら開始
-    if($(window).scrollTop() > fixTop ) {    //スクロール時の縦座標がメニューの縦座標以上なら…
+    if($(window).scrollTop() > fixTop-90 ) {    //スクロール時の縦座標がメニューの縦座標以上なら…
       fix.addClass("fixed");
+      fix.removeClass("clear-fix");
     } else {
       fix.removeClass("fixed");
+      fix.addClass("clear-fix");
     }
   });
 });
