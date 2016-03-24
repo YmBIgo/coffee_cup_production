7a6e9a72c367ea10cecb73d9aa5177931a6e3efd
Kazuya <IgYmB0218LP@gmail.com>
2016-03-25 08:22:53 +0900

    page11 finish

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get "/pages" => "pages#index"
 
+  get "/codes" => "codes#index"
+
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
