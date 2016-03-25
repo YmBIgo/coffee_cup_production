a04186a72b327179bba8aa1ba78b5bcfe3ab8d6a
Kazuya <IgYmB0218LP@gmail.com>
2016-03-25 20:51:47 +0900

    add code10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
+  http_basic_authenticate_with :name => ENV['EDIT_USER'], :password => ENV['EDIT_PASS']
+
   def show
     render template: "pages/#{params[:page]}"
   end
