5607942c875fc6f68185c276679a1d59964306eb
Kazuya <IgYmB0218LP@gmail.com>
2016-03-27 13:41:25 +0900

    code11 finish

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,5 +1,7 @@
 class CodesController < ApplicationController
 
+  http_basic_authenticate_with :name => ENV['EDIT_USER'], :password => ENV['EDIT_PASS']
+
   def show
     render template: "codes/#{params[:page]}"
   end
