21d31223b167412b0b3f9b88183413abaea2e145
Kazuya <IgYmB0218LP@gmail.com>
2016-05-01 00:32:36 +0900

    change fixjs

--- app/assets/javascripts/main/style.js
+++ app/assets/javascripts/main/style.js
@@ -8,3 +8,30 @@ $(function(){
         return false;
     });
 });
+
+      var x = 133;
+    function larger()  {
+      x *= 1.1;
+      var selected_doc = document.getElementsByTagName("h4");
+      for(var i = 0; i < selected_doc.length; i++){
+        selected_doc[i].style.fontSize = x+"%";
+      }
+    }
+    function smaller() {
+      x /= 1.1;
+      var selected_doc = document.getElementsByTagName("h4");
+      for(var i = 0; i < selected_doc.length; i++){
+        selected_doc[i].style.fontSize = x+"%";
+      }
+    }
+
+    $(function(){
+        $('a[href^=#]').click(function(){ 
+            var speed = 500; //移動完了までの時間(sec)を指定
+            var href= $(this).attr("href"); 
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+            $("html, body").animate({scrollTop:position}, speed, "swing");
+            return false;
+        });
+    });
