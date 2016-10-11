f185b1df8acef214470896cfa4bdbe39811dfcc2
Kazuya <IgYmB0218LP@gmail.com>
2016-10-11 19:01:42 +0900

    fix user_agent

--- db/migrate/20161011094904_change_column_viewlist_user_agent.rb
+++ db/migrate/20161011094904_change_column_viewlist_user_agent.rb
@@ -2,12 +2,12 @@ class ChangeColumnViewlistUserAgent < ActiveRecord::Migration
 
   def self.up
     change_column :viewlists, :referer,     :string, :default => "no", :null => false
-    add_column    :viewlists, :user_agent,  :string, :null => false
+    add_column    :viewlists, :user_agent,  :string, :default => "no data", :null => false
   end
 
   def self.down
     change_column :viewlists, :referer,    :string, :default => "no"
-    remove_column :viewlists, :user_agent, :string, :null => false
+    remove_column :viewlists, :user_agent, :string, :default => "no data", :null => false
   end
 
 end
