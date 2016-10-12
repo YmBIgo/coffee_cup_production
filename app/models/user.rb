e7799dad62a332db2d3ff2fe2c6f816a90c87da4
Kazuya <IgYmB0218LP@gmail.com>
2016-10-12 12:50:14 +0900

    implement company check

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :viewlists
+  has_one  :company
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
